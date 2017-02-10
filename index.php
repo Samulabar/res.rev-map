a14c7f509f68789ee47966e0d71eb0f7772c1b5b
Samulabar <Samulabar@mail.ru>
2017-02-10 23:32:10 +0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+	<head> 
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+		<style>
+		    .map{ height: calc(100vh - 70px); }
+    	</style>
+		
+	<script type="text/javascript" src="//vk.com/js/api/openapi.js?139"></script>
+
+<script type="text/javascript">
+  VK.init({apiId: 5868926, onlyWidgets: true});
+</script>
+		
+	</head>
+
+	<body>
+
+<div id="vk_comments"></div>
+<script type="text/javascript">
+VK.Widgets.Comments("vk_comments", {limit: 10, height: "665", attach: "*"});
+</script>
+		
+	</body>
+</html>
